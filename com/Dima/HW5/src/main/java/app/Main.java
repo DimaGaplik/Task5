b12dd8bc902475f90469d3aa2604b8cc318b0cd2
Dima Gaplik <dimagaplik575@gmail.com>
2024-04-29 19:06:00 +0300

    Init commit

---
+++ com/Dima/HW5/src/main/java/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your revenue:");
+        double revenue = scanner.nextDouble();
+        double tax = 0;
+
+        if (revenue <= 10000) {
+            tax = revenue * 0.025;
+        } else if (revenue <= 25000) {
+            tax = 10000 * 0.025 + (revenue - 10000) * 0.043;
+        } else if (revenue > 25000) {
+            tax = revenue * 0.067;
+        }
+        System.out.printf("Налог для дохода %.2f грн: %.2f грн%n", revenue, tax);
+
+        scanner.close();
+    }
+}
